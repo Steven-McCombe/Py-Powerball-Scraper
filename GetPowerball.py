e9c8176e630016e9aa0db64217103ae518588842
steven mccombe <smccombe93@gmail.com>
2023-10-11 14:45:13 -0400

    seperated the cells for the winning numbers

--- GetPowerball.py
+++ GetPowerball.py
@@ -1,28 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+from urllib.parse import parse_qs, urlparse
 
-page_to_scrape = requests.get("https://www.powerball.com/draw-result?gc=powerball&date=2023-10-09")
+url = "https://www.powerball.com/draw-result?gc=powerball&date=2023-10-09"
+page_to_scrape = requests.get(url)
 soup = BeautifulSoup(page_to_scrape.text, "html.parser")
-winningNumbers = soup.findAll("div", attrs={"class": "form-control col white-balls item-powerball"})
+winningNumbers = [num.text for num in soup.findAll("div", attrs={"class": "form-control col white-balls item-powerball"})]
 winningPowerball = soup.find("div", attrs={"class": "form-control col powerball item-powerball"})
+if winningPowerball:
+    winningPowerball = winningPowerball.text
+else:
+    winningPowerball = "N/A"
 
-file = open("scraped_winningNumbers.csv", "w")
+# Extract date from URL
+parsed_url = urlparse(url)
+date = parse_qs(parsed_url.query)['date'][0]
+
+file = open("scraped_winningNumbers.csv", "w", newline='')
 writer = csv.writer(file)
 
 # Write the header row to the CSV
-writer.writerow(["WINNINGNUMBERS", "winningPowerball"])
+writer.writerow(["Date", "Winning Number 1", "Winning Number 2", "Winning Number 3", "Winning Number 4", "Winning Number 5", "Powerball"])
 
-# Join the winning numbers into a single string
-winningNumbers_str = ' '.join([num.text for num in winningNumbers])
+# Create a row combining the date, winning numbers, and Powerball number
+row = [date] + winningNumbers + [winningPowerball]
 
-# Get the winning Powerball number
-winningPowerball_str = winningPowerball.text if winningPowerball else "N/A"
+# Print the row (optional)
+print(row)
 
-# Print the winning numbers and Powerball number
-print(winningNumbers_str + " - " + winningPowerball_str)
-
-# Write the winning numbers and Powerball number to the CSV
-writer.writerow([winningNumbers_str, winningPowerball_str])
+# Write the row to the CSV
+writer.writerow(row)
 
 file.close()
