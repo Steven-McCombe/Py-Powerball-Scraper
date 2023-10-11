55e985dafae7c3eb7c48775ddeb57dcc98df0412
steven mccombe <smccombe93@gmail.com>
2023-10-11 15:33:15 -0400

    updated to iterate through all mondays,wednesdays and saturdays for the last year

--- GetPowerball.py
+++ GetPowerball.py
@@ -1,35 +1,33 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 import csv
-from urllib.parse import parse_qs, urlparse
+from datetime import datetime, timedelta
+from dateutil import rrule
 
-url = "https://www.powerball.com/draw-result?gc=powerball&date=2023-10-09"
-page_to_scrape = requests.get(url)
-soup = BeautifulSoup(page_to_scrape.text, "html.parser")
-winningNumbers = [num.text for num in soup.findAll("div", attrs={"class": "form-control col white-balls item-powerball"})]
-winningPowerball = soup.find("div", attrs={"class": "form-control col powerball item-powerball"})
-if winningPowerball:
-    winningPowerball = winningPowerball.text
-else:
-    winningPowerball = "N/A"
+# Function to scrape Powerball data for a specific date
+def scrape_powerball(date):
+    url = f"https://www.powerball.com/draw-result?gc=powerball&date={date}"
+    page_to_scrape = requests.get(url)
+    soup = BeautifulSoup(page_to_scrape.text, "html.parser")
+    winningNumbers = [num.text for num in soup.findAll("div", attrs={"class": "form-control col white-balls item-powerball"})]
+    winningPowerball = soup.find("div", attrs={"class": "form-control col powerball item-powerball"})
+    winningPowerball = winningPowerball.text if winningPowerball else "N/A"
+    return [date] + winningNumbers + [winningPowerball]
 
-# Extract date from URL
-parsed_url = urlparse(url)
-date = parse_qs(parsed_url.query)['date'][0]
+# Set the start and end dates for the 10-year period
+end_date = datetime.now()
+start_date = end_date - timedelta(days=10*365)  # Approximately 10 years
+
+# Generate dates for every Monday, Wednesday, and Saturday between start_date and end_date
+dates = list(rrule.rrule(rrule.WEEKLY, byweekday=(rrule.MO, rrule.WE, rrule.SA), dtstart=start_date, until=end_date))
 
 file = open("scraped_winningNumbers.csv", "w", newline='')
 writer = csv.writer(file)
 
-# Write the header row to the CSV
-writer.writerow(["Date", "Winning Number 1", "Winning Number 2", "Winning Number 3", "Winning Number 4", "Winning Number 5", "Powerball"])
-
-# Create a row combining the date, winning numbers, and Powerball number
-row = [date] + winningNumbers + [winningPowerball]
-
-# Print the row (optional)
-print(row)
-
-# Write the row to the CSV
-writer.writerow(row)
+for date in dates:
+    date_str = date.strftime('%Y-%m-%d')
+    print(f"Scraping data for {date_str}")  # Optional: print progress
+    row = scrape_powerball(date_str)
+    writer.writerow(row)
 
 file.close()
